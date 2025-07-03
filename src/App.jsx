6cd63eb18bdefd31f3427829570291f2b50620be
priyanshukanyal <kanyalpriyanshu@gmail.com>
2025-07-03 12:45:56 +0530

    frontend fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,6 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Login from "./pages/Login";
-// import OtpPage from "./pages/OtpPage";
-// import Home from "./pages/Home";
-
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<Login />} />
-//         <Route path="/otp" element={<OtpPage />} />
-//         <Route path="/home" element={<Home />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
 
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import DashboardLayout from "./layouts/DashboardLayout"; // layout with sidebar
 import Home from "./pages/Home";
@@ -50,41 +31,39 @@ import AdminProfile from "./pages/AdminProfile";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Public routes */}
-        <Route path="/" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/change-password" element={<ChangePassword />} />
+    <Routes>
+      {/* Public routes */}
+      <Route path="/" element={<Login />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/change-password" element={<ChangePassword />} />
 
-        {/* Protected/Admin routes with sidebar */}
-        <Route element={<DashboardLayout />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/CommunityContent" element={<CommunityContent />} />
-          <Route path="/ProgramManagement" element={<ProgramManagement />} />
-          <Route path="/Challenges" element={<Challenges />} />
-          <Route path="/ClassModeration" element={<ClassModeration />} />
-          <Route path="/EventsPage" element={<EventsPage />} />
-          <Route path="/MarketplacePage" element={<MarketplacePage />} />
-          <Route path="/AdsManagement" element={<AdsManagement />} />
-          <Route path="/NotificationPage" element={<NotificationPage />} />
-          <Route path="/Dashboard" element={<Dashboard />} />
-          <Route path="/SettingsPage" element={<SettingsPage />} />
-          <Route path="/users/Dancers" element={<DancersPage />} />
-          <Route path="/users/Professors" element={<ProfessorsPage />} />
-          <Route path="/PayoutsPage" element={<PayoutsPage />} />
-          <Route path="/AccessLogs" element={<AccessLogs />} />
-          <Route path="/VideoPrograms" element={<VideoPrograms />} />
-          <Route path="/CreateChallenge" element={<CreateChallenge />} />
-          <Route path="/settings/app-branding" element={<AppBrandingPage />} />
-          <Route path="/settings/subscription-plans" element={<SubscriptionPlansPage />} />
-          <Route path="/settings/legal-content" element={<LegalContentPage />} />
-          <Route path="/settings/app-version" element={<AppVersionInfo />} />
-          <Route path="/admin-profile" element={<AdminProfile />} />
-        </Route>
-      </Routes>
-    </Router>
+      {/* Protected/Admin routes with sidebar */}
+      <Route element={<DashboardLayout />}>
+        <Route path="/home" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/CommunityContent" element={<CommunityContent />} />
+        <Route path="/ProgramManagement" element={<ProgramManagement />} />
+        <Route path="/Challenges" element={<Challenges />} />
+        <Route path="/ClassModeration" element={<ClassModeration />} />
+        <Route path="/EventsPage" element={<EventsPage />} />
+        <Route path="/MarketplacePage" element={<MarketplacePage />} />
+        <Route path="/AdsManagement" element={<AdsManagement />} />
+        <Route path="/NotificationPage" element={<NotificationPage />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
+        <Route path="/SettingsPage" element={<SettingsPage />} />
+        <Route path="/users/Dancers" element={<DancersPage />} />
+        <Route path="/users/Professors" element={<ProfessorsPage />} />
+        <Route path="/PayoutsPage" element={<PayoutsPage />} />
+        <Route path="/AccessLogs" element={<AccessLogs />} />
+        <Route path="/VideoPrograms" element={<VideoPrograms />} />
+        <Route path="/CreateChallenge" element={<CreateChallenge />} />
+        <Route path="/settings/app-branding" element={<AppBrandingPage />} />
+        <Route path="/settings/subscription-plans" element={<SubscriptionPlansPage />} />
+        <Route path="/settings/legal-content" element={<LegalContentPage />} />
+        <Route path="/settings/app-version" element={<AppVersionInfo />} />
+        <Route path="/admin-profile" element={<AdminProfile />} />
+      </Route>
+    </Routes>
   );
 }
 
